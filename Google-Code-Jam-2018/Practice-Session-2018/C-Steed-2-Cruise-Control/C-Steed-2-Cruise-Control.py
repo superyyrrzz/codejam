43ff260852e44a09294e6776e0439eba9bed0bd0
renzeyu <renzeyu@microsoft.com>
2018-10-22 23:01:14 +0800

    sove Cruise Control

---
+++ Google-Code-Jam-2018/Practice-Session-2018/C-Steed-2-Cruise-Control/C-Steed-2-Cruise-Control.py
@@ -0,0 +1,23 @@
+# Problem C: Steed 2: Cruise Control
+# URL: https://codejam.withgoogle.com/2018/challenges/0000000000000130/dashboard/0000000000000524
+
+
+def ChooseSpeed(d, n, k, s):
+    tmax = 0
+    for i in range(n):
+        t = (d - k[i]) / s[i]
+        tmax = max(t, tmax)
+    return d / tmax
+
+
+t = int(input())
+for i in range(1, t + 1):
+    [d, n] = [int(c) for c in input().split()]
+    k = []
+    s = []
+    for j in range(n):
+        [kt, st] = [int(c) for c in input().split()]
+        k.append(kt)
+        s.append(st)
+    speed = ChooseSpeed(d, n, k, s)
+    print("Case #{}: ".format(i) + str(round(speed, 6)))
