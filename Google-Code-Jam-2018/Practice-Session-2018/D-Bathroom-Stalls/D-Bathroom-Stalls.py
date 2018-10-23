2b690aae6713f83ed639cbd74eebf367d9b1e5e6
renzeyu <renzeyu@microsoft.com>
2018-10-24 00:33:40 +0800

    solve Bathroom Stalls

--- Google-Code-Jam-2018/Practice-Session-2018/D-Bathroom-Stalls/D-Bathroom-Stalls.py
+++ Google-Code-Jam-2018/Practice-Session-2018/D-Bathroom-Stalls/D-Bathroom-Stalls.py
@@ -8,13 +8,19 @@ import math
 
 def ChooseBathroom(n, k):
     max_heap = [-n]
-    for _ in range(k):
+    count_dict = {n : 1}
+    p = 0
+    while True:
         max_spaces = -heapq.heappop(max_heap)
         max = math.ceil((max_spaces - 1) / 2)
         min = math.floor((max_spaces - 1) / 2)
+        p += count_dict[n]
+        if p >= k:
+            return (max, min)
         heapq.heappush(max_heap, -max)
+        count_dict[max] = count_dict.setdefault(max, 0) + count_dict[n]
         heapq.heappush(max_heap, -min)
-    return (max, min)
+        count_dict[min] = count_dict.setdefault(min, 0) + count_dict[n]
 
 
 t = int(input())
